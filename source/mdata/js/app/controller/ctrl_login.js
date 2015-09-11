b827ace74092ed863419a6a5d7b9a00847f20dbc
xiuling <644961699@qq.com>
2015-09-11 12:20:04 +0800

    sss

--- source/mdata/js/app/controller/ctrl_login.js
+++ source/mdata/js/app/controller/ctrl_login.js
@@ -18,12 +18,12 @@ oasgames.mdataPanelControllers.controller('MdataLoginCtrl', [
         $scope.blur = function (type, $errors) {
             var errorInfo = {
                 account: {
-                    required: '请输入账号',
-                    pattern: '账号格式错误'
+                    required: 'E-mail must not be empty',
+                    pattern: 'This user does not exist'
                 },
                 password: {
-                    required: '请输入密码',
-                    pattern: '密码格式错误'
+                    required: 'Password must not be empty',
+                    pattern: 'Incorrect Password '
                 }
             };
 
@@ -87,66 +87,68 @@ oasgames.mdataPanelControllers.controller('MdataChangePasswordCtrl', [
     '$rootScope',
     '$http',
     'ApiCtrl',
-    //'HeaderCtrl',
     function ($scope, $rootScope, $http, ApiCtrl) {
         $scope.tpassword = new Tooltip({'position':'rc'}).getNewTooltip();
         $scope.tnewPassword = new Tooltip({'position':'rc'}).getNewTooltip();
         $scope.treNewPassword = new Tooltip({'position':'rc'}).getNewTooltip();
+        //错误提示
+        $scope.showError = function(type, pError){
+            $scope[type + 'Error'] = true;
+            $scope['t'+type].errorType = type;
+            $scope['t'+type].setContent(pError);
+            $scope['t'+type].setPosition('.text-' + type, $scope['t'+type].toolTipLooks);
+            $scope['t'+type].toolTipLooks.css({'color': 'rgba(255, 0, 0, 0.7)'});
+            $scope['t'+type].show();
+        }
         //表单失去焦点时错误验证
         $scope.blur = function (type, $errors) {
             var errorInfo = {
                 password: {
-                    required: '请输入旧密码',
-                    pattern: '格式错误，长度为6-20的字母、数字或特殊字符'
+                    required: 'Password must not be empty',
+                    error: 'Incorrect Password'
                 },
                 newPassword: {
-                    required: '请输入新密码',
-                    pattern: '格式错误，长度为6-20的字母或数字'
+                    required: 'Password must not be empty',
+                    pattern: 'Incorrect Format,Password must be 6-20 characters with English letters and numbers',
+                    noSame: 'New Password must not be same as Old Password'
                 },
                 reNewPassword:{
-                    required: '请重新输入新密码',
-                    pattern: '格式错误，长度为6-20的字母或数字'
+                    required: 'Password must not be empty',
+                    pattern: 'Incorrect Format,Password must be 6-20 characters with English letters and numbers',
+                    mustSame: 'Passwords do not match'
                 },
             };
+            //格式不正确
             for(var $error in $errors) {
                 if($errors[$error]) {
-                    $scope[type + 'Error'] = true;
-                    $scope['t'+type].errorType = type;
-                    $scope['t'+type].setContent(errorInfo[type][$error]);
-                    $scope['t'+type].setPosition('.text-' + type, $scope['t'+type].toolTipLooks);
-                    $scope['t'+type].toolTipLooks.css({'color': 'rgba(255, 0, 0, 0.7)'});
-                    $scope['t'+type].show();
+                    $scope.showError(type, errorInfo[type][$error]);
                     return;
                 }
             }
-            console.log($scope.newPassword +"=="+ $scope.password +"&&"+ type)
-            if($scope.newPassword == $scope.password && type == "newPassword"){
-                $scope['newPasswordError'] = true;
-                $scope['tnewPassword'].errorType = type;
-                $scope['tnewPassword'].setContent("New Password must not be same as Old Password");
-                $scope['tnewPassword'].setPosition('.text-newPassword', $scope['tnewPassword'].toolTipLooks);
-                $scope['tnewPassword'].toolTipLooks.css({'color': 'rgba(255, 0, 0, 0.7)'});
-                $scope['tnewPassword'].show();
+            //新旧密码相同
+            if($scope.newPassword == $scope.password && type == "newPassword" ){
+                $scope.showError('newPassword', errorInfo.newPassword.noSame);
             }
-            if($scope.newPassword != $scope.reNewPassword && type == "reNewPassword"){
-                $scope['reNewPasswordError'] = true;
-                $scope['treNewPassword'].errorType = type;
-                $scope['treNewPassword'].setContent("Passwords do not match");
-                $scope['treNewPassword'].setPosition('.text-reNewPassword', $scope['treNewPassword'].toolTipLooks);
-                $scope['treNewPassword'].toolTipLooks.css({'color': 'rgba(255, 0, 0, 0.7)'});
-                $scope['treNewPassword'].show();
-            }
-
-            $scope[type + 'Error'] = false;
+            //重新输入密码不一致
+            if($scope.newPassword != $scope.reNewPassword && (type == "reNewPassword" || type == "newPassword")){
+                $scope.showError('reNewPassword', errorInfo.reNewPassword.mustSame);
+            }   
+            //重新输入新密码正确
+            if($scope.newPassword == $scope.reNewPassword && type == "newPassword" ){
+                if($scope.treNewPassword.errorType == 'reNewPassword') {
+                    $scope.treNewPassword.hide();
+                    $scope.reNewPasswordError = false;
+                }
+            }  
         };
-        //表单失去焦点时错误验证
+        //表单失去焦点时清除错误
         $scope.focus = function (type) {
             $scope[type + 'Error'] = false;
             if($scope['t'+type].errorType == type) {
                 $scope['t'+type].hide();
             }
         };
-        //修改密码
+        //修改密码提交
         $scope.submit = function () {
             var api = ApiCtrl.get('login');
             //验证通过、新旧密码不一致并且新密码相同
@@ -154,9 +156,7 @@ oasgames.mdataPanelControllers.controller('MdataChangePasswordCtrl', [
                 $http.get(api).success(function (result) {
 
                     if(result.code == 200) {
-                        //记录登陆状态
-                        $rootScope.user['logined'] = false;
-                        $scope.$emit('logout');
+                        Ui.alert('Your password has been changed successfully');
                     }
 
                 }).error(function (status) {
