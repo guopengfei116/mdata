12d7b1aad0602f1cb95a2b2c07752d2d87518814
郭朋飞 <guopengfei@pengfei.local>
2015-09-21 18:11:02 +0800

    report

--- source/mdata/js/constant/constant.js
+++ source/mdata/js/constant/constant.js
@@ -24,3 +24,15 @@ oasgames.mdataConstant.constant("TIME_ZONE", [
     { key : 'USA', value : 'America/New_York' }
 ]);
 
+/*
+ * report日期范围
+ * */
+oasgames.mdataConstant.constant("REPORT_DATE_RANGE", [
+    { meaning : '今天', value : 0 },
+    { meaning : '昨天', value : 1 },
+    { meaning : '过去7天', value : 7 },
+    { meaning : '上周', value : '7L' },
+    { meaning : '过去30天', value : 30 },
+    { meaning : '上月', value : '30L' }
+]);
+
--- source/mdata/js/controller/report.js
+++ source/mdata/js/controller/report.js
@@ -156,7 +156,37 @@ oasgames.mdataControllers.controller('reportEditCtrl', [
  * */
 oasgames.mdataControllers.controller('reportViewCtrl', [
     '$scope',
-    function ($scope) {
+    '$route',
+    'REPORT_DATE_RANGE',
+    'Report',
+    function ($scope, $route, reportDateRanges, Report) {
 
+        // report日期范围
+        $scope.reportDateRanges = {};
+
+        // 当前report的数据
+        $scope.reportSourceData = {};
+
+        // 当前编辑的reportId
+        $scope.reportId = $route.current.params.reportId;
+
+        if($scope.reportId) {
+            $scope.reportId = 'report_view';
+        }
+
+        // get report数据
+        Report.get(
+            {reportId: $scope.reportId},
+            function (result) {
+                if(result && result.code == 200) {
+                    $scope.reportSourceData = result.data;
+                }else {
+                    Ui.alert(result.msg);
+                }
+            },
+            function () {
+                Ui.alert('网络错误');
+            }
+        );
     }
 ]);
