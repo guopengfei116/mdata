9c5c735ccb86e4b3bd70e08f55ca02b402dcb2b5
郭朋飞 <guopengfei@pengfei.local>
2015-10-09 16:43:49 +0800

    cookie

--- source/mdata/js/util/cookie.js
+++ source/mdata/js/util/cookie.js
@@ -8,15 +8,48 @@
          * @param {String} value
          * @param {Number} expire
          */
-        setCookie : function (key, value, expire)	{
+        oldSetCookie : function (key, value, expire)	{
             var DAY = 24 * 60 * 60 * 1000,
                 now = new Date(),
-                exp = expire ? expire : 30;
+                exp = expire ? expire : 0;
 
             now.setTime(now.getTime() + exp * DAY);
             document.cookie = key + "=" + encodeURIComponent(value) + "; path=/" + "; expires=" + now.toGMTString();
         },
-        /**
+
+        setCookie : function (key, value, options) {
+            options = $.extend({},{
+                domain : '',
+                path : '/'
+            }, options);
+
+            //删除cookie操作处理
+            if (value === null) {
+                options.expires = -1;
+            }
+
+            //设置过期时间
+            if (typeof options.expires === 'number') {
+                var seconds = options.expires, t = options.expires = new Date();
+                t.setTime(t.getTime() + seconds*1000*60*60);
+            }
+
+            //强制转换为字符串格式
+            value = '' + value;
+
+            //设置cookie信息
+            return (document.cookie = [
+                encodeURIComponent(key), '=',
+                options.raw
+                ? value : encodeURIComponent(value),
+                options.expires ? '; expires=' + options.expires.toUTCString() : '',
+                options.path ? '; path=' + options.path : '',
+                options.domain ? '; domain=' + options.domain : '',
+                options.secure ? '; secure' : ''
+            ].join(''));
+        },
+
+         /**
          * @method getCookie
          * @description 读取cookie
          * @param {String} key
